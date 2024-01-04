743ae33b86c89d650f55b93680697bdb9edac3b6
Igor Barreto <igorof06@outlook.com>
2024-01-04 10:33:03 -0300

    configure tests variables

---
+++ personCRUD/src/test/java/com/igor/personCRUD/Config/TestsConfig.java
@@ -0,0 +1,12 @@
+package com.igor.personCRUD.Config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestsConfig {
+
+    public static final int SERVER_PORT = 8888;
+    public static final String CONTENT_TYPE_JSON = "application/json";
+
+
+}
